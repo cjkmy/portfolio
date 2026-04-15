1c2238bf55ea42e8cd40e51ca26378d4f7679195
cjkmy <ckimayong@ucsd.edu>
2026-04-15 01:37:07 -0700

    j

---
+++ global.js
@@ -0,0 +1,29 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const navLinks = $$("nav a");
+
+//let currentLink = navLinks.find(
+  //(a) => a.host === location.host && a.pathname === location.pathname,
+//);
+
+//currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'projects/', title: 'Resume' },
+    { url: 'projects/', title: 'Contact' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', '<a href="${url}">${title}</a>');
+}
