aac7afa4cdc84947fc9a32a0cee460dc1b92d84b
cjkmy <ckimayong@ucsd.edu>
2026-04-24 00:51:12 -0700

    Update global.js

--- global.js
+++ global.js
@@ -109,3 +109,30 @@ export async function fetchJSON(url) {
 }
 
 window.fetchJSON = fetchJSON;
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  if (!containerElement) {
+    console.error('Container element not found');
+    return;
+  }
+
+  const validHeadings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+  if (!validHeadings.includes(headingLevel)) {
+    console.warn(`${headingLevel} is not valid. Using h2 instead.`);
+    headingLevel = 'h2';
+  }
+
+  containerElement.innerHTML = '';
+  projects.forEach((project) => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  });
+}
